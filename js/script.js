aa65a00f3135d23351c2ccd097cd2fc1d486e5ac
Katie <p.sneg@inbox.ru>
2019-11-20 21:33:28 +0400

    store last changes in localstorage & better keys handling

--- js/script.js
+++ js/script.js
@@ -31,49 +31,115 @@ function init () {
 }
 
 $(document).ready(() => {
-  init();
   const storage = JSON.parse(localStorage.getItem('storage'));
-  console.log(storage)
+  if (storage.length && storage.map(x => x).join('').trim().length) {
+    $('.container').empty();
+    for (let i = 0; i < storage.length; i++) {
+      $('.container').append(`
+        <div class="block">
+          <textarea rows="1">${storage[i]}</textarea>
+          <span>0</span>
+        </div>
+      `)
+    }
+  }
+  init();
   setInterval(() => {
     let text = [];
     const areas = document.querySelectorAll('textarea');
     areas.forEach(area => text.push($(area).val()));
     text = JSON.stringify(text);
     localStorage.setItem('storage', text)
-  }, 10000);
+  }, 5000);
 })
 
 $(document).on('keydown', (e) => {
-  if (e.keyCode === 8 && !$(e.target).val()) {
-    $(e.target).parent().prev().children('textarea').focus();
-    if ($(e.target).parent().prev().length) {
-      $(e.target).parent().remove();
-    }
-    return false
-  }
-  if (e.keyCode === 38) {
-    e.preventDefault();
-    $(e.target).parent().prev().children('textarea').focus();
+  const parent = $(e.target).parent();
+  const selectionEnd = $(e.target)[0].selectionEnd;
+  const selectionStart = $(e.target)[0].selectionStart;
+  if (
+    e.keyCode === 8 && // backspace
+    parent.prev().children('textarea').length &&
+    selectionEnd === selectionStart &&
+    selectionStart === 0
+  ) {
+    const prev = parent.prev().children('textarea');
+    const start = prev.val().length;
+    prev.focus();
+    prev.val(prev.val() + $(e.target).val());
+    prev[0].selectionStart = start;
+    prev[0].selectionEnd = start;
+    parent.remove();
     return false;
   }
-  if (e.keyCode === 40) {
-    e.preventDefault();
-    $(e.target).parent().next().children('textarea').focus();
+  if (
+    e.keyCode === 46 && // delete
+    selectionEnd === selectionStart &&
+    selectionEnd === $(e.target).val().length &&
+    parent.next().children('textarea').length
+  ) {
+    const start = selectionStart;
+    $(e.target).val($(e.target).val() + parent.next().children('textarea').val());
+    $(e.target)[0].selectionEnd = start;
+    $(e.target)[0].selectionStart = start;  
+    parent.next().remove();
     return false;
   }
-  if (e.keyCode === 13) {
+  if (
+    e.keyCode === 37 && // arrow left
+    selectionEnd === selectionStart &&
+    selectionStart === 0 &&
+    parent.prev().children('textarea').length
+  ) {
+    $(e.target).blur();
+    const prev = parent.prev().children('textarea');
+    const place = prev.val().length;
+    prev.focus();
+    prev[0].selectionStart = place;
+    prev[0].selectionEnd = place;
+    return false;
+  }
+  if (e.keyCode === 38) { // arrow top
+    e.preventDefault();
+    parent.prev().children('textarea').focus();
+    return false;
+  }
+  if (
+    e.keyCode === 39 && // arrow right
+    selectionEnd === $(e.target).val().length &&
+    parent.next().children('textarea').length
+  ) {
+    $(e.target).blur();
+    const next = parent.next().children('textarea');
+    next.focus();
+    next[0].selectionStart = 0;
+    next[0].selectionEnd = 0;
+    return false;
+  }
+  if (e.keyCode === 40) { // arrow bottom
+    e.preventDefault();
+    parent.next().children('textarea').focus();
+    return false;
+  }
+  if (e.keyCode === 13 && selectionEnd === selectionStart) { // enter
     e.preventDefault();
     $(e.target).blur();
-    if (!$(e.target).parent().next().children('textarea').length) {
-      $('.container').append(`
-        <div class="block">
-          <textarea rows="1"></textarea>
-          <span>0</span>
-        </div>
-      `);
-      init();
+    $(`
+      <div class="block">
+        <textarea rows="1"></textarea>
+        <span>0</span>
+      </div>
+    `).insertAfter(parent);
+    init();
+    const next = parent.next().children('textarea');
+    next.focus();
+    if (selectionEnd !== $(e.target).val().length) {
+      const cut = $(e.target).val().slice(selectionStart);
+      $(e.target).val($(e.target).val().slice(0, selectionStart));
+      next.val(cut);
+      next[0].selectionStart = 0;
+      next[0].selectionEnd = 0;
     }
-    $(e.target).parent().next().children('textarea').focus();
     return false;
   }
 })
